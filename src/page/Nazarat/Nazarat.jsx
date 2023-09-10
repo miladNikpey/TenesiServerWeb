476853b6cf7ce21e2a91e095b9b017eadbe71b30
miladNikpey <miladnik5040@gmail.com>
2023-09-10 19:05:21 +0430

    create Nazarat component

---
+++ src/page/Nazarat/Nazarat.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import styles from "./Nazarat.module.scss";
+import { Carousel } from "antd";
+
+
+const Nazarat = () => {
+  return (
+    <div>
+      <div className={styles.nazarBackgroud}>
+        <div className={styles.background}>
+          <div className={styles.titr}>
+            <h1>نظرات برخی از کاربران و مشتریان ما</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nazarat;
